ea078db27023dc4d70793c22c7c9433ad12de433
DaddyMillz <oluwadamilareshadrach@gmail.com>
2025-10-03 14:53:43 +0100

    added resposiveness and hambugger with js

---
+++ script.js
@@ -0,0 +1,4 @@
+const navToggle = document.getElementById (`navToggle`)
+const navLinks = document.getElementById (`navLinks`)
+
+navToggle.addEventListener('click', () => {navLinks.classList.toggle('show');})
